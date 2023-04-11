0496915c42ac7bd909e31387340553f33e7d4713
João Pedro <joao.fonseca@aluno.faculdadeimpacta.com.br>
2023-04-11 10:42:32 -0300

    Adicionado Docker e criado imagem

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return (f' Cleriston Junior RA: 22201072 \n Joao Pedro RA:2201068 \n Mateus Pereira Jardim RA: 2201058 \n Guilherme Pereira RA:2201325')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+print(' Cleriston Junior RA: 22201072 \n Joao Pedro RA:2201068 \n Mateus Pereira Jardim RA: 2201058 \n Guilherme Pereira RA:2201325')
 
 
 

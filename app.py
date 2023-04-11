8634efdb7a0766e783fe957608cf7bec35d0d1d9
Cleriston-Junior-Impacta <cleriston.junior@aluno.faculdadeimpacta.com.br>
2023-04-11 09:07:07 -0300

    resolvido erro

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return (f'Cleriston Junior RA: 22201072', end=" ", 'Joao Pedro RA:2201068 \n Mateus Pereira Jardim RA: 2201058 \n Guilherme Pereira RA:2201325')
+    return (f' Cleriston Junior RA: 22201072 \n Joao Pedro RA:2201068 \n Mateus Pereira Jardim RA: 2201058 \n Guilherme Pereira RA:2201325')
 
 if __name__ == "__main__":
     app.run(debug=True)

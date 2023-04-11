8de8dcc0afaaa2e04612f679a2d91dd8ea0dca1c
Cleriston-Junior-Impacta <cleriston.junior@aluno.faculdadeimpacta.com.br>
2023-04-11 09:01:30 -0300

    Adicinado nomes e RAs do integrantes do grupo

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Ac-02')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return (f'Cleriston Junior RA: 22201072', end=" ", 'Joao Pedro RA:2201068 \n Mateus Pereira Jardim RA: 2201058 \n Guilherme Pereira RA:2201325')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
